7371542b69a1dc7831d58914b2f4253b60d36073
AvramCatalin <49835245+AvramCatalin@users.noreply.github.com>
2019-04-22 20:50:37 +0300

    Lab 3 fully functional

---
+++ Lab 3/MyDatabase/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace MyDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox_Univ = new System.Windows.Forms.ListBox();
+            this.listBox_Fac = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_City = new System.Windows.Forms.TextBox();
+            this.textBox_CodeUniv = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_ModCity = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_ModCodeUniv = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_Modify = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numeFacDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.facultatiBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new MyDatabase.Database1DataSet();
+            this.database1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.facultatiTableAdapter = new MyDatabase.Database1DataSetTableAdapters.FacultatiTableAdapter();
+            this.button_update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facultatiBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Universitati";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // listBox_Univ
+            // 
+            this.listBox_Univ.FormattingEnabled = true;
+            this.listBox_Univ.HorizontalScrollbar = true;
+            this.listBox_Univ.Location = new System.Drawing.Point(12, 27);
+            this.listBox_Univ.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox_Univ.Name = "listBox_Univ";
+            this.listBox_Univ.Size = new System.Drawing.Size(170, 160);
+            this.listBox_Univ.TabIndex = 1;
+            this.listBox_Univ.SelectedIndexChanged += new System.EventHandler(this.ListBox_Univ_SelectedIndexChanged_1);
+            // 
+            // listBox_Fac
+            // 
+            this.listBox_Fac.FormattingEnabled = true;
+            this.listBox_Fac.Location = new System.Drawing.Point(194, 27);
+            this.listBox_Fac.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox_Fac.Name = "listBox_Fac";
+            this.listBox_Fac.Size = new System.Drawing.Size(230, 69);
+            this.listBox_Fac.TabIndex = 2;
+            this.listBox_Fac.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Facultati";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 98);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Oras:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(191, 143);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Cod Univ:";
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // textBox_City
+            // 
+            this.textBox_City.Enabled = false;
+            this.textBox_City.Location = new System.Drawing.Point(194, 114);
+            this.textBox_City.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_City.Name = "textBox_City";
+            this.textBox_City.Size = new System.Drawing.Size(91, 20);
+            this.textBox_City.TabIndex = 6;
+            // 
+            // textBox_CodeUniv
+            // 
+            this.textBox_CodeUniv.Enabled = false;
+            this.textBox_CodeUniv.Location = new System.Drawing.Point(194, 159);
+            this.textBox_CodeUniv.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_CodeUniv.Name = "textBox_CodeUniv";
+            this.textBox_CodeUniv.Size = new System.Drawing.Size(91, 20);
+            this.textBox_CodeUniv.TabIndex = 7;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(9, 217);
+            this.textBox_Name.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(174, 20);
+            this.textBox_Name.TabIndex = 8;
+            this.textBox_Name.TextChanged += new System.EventHandler(this.TextBox_Name_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 201);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Numele Universitatii";
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // textBox_ModCity
+            // 
+            this.textBox_ModCity.Location = new System.Drawing.Point(194, 217);
+            this.textBox_ModCity.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_ModCity.Name = "textBox_ModCity";
+            this.textBox_ModCity.Size = new System.Drawing.Size(91, 20);
+            this.textBox_ModCity.TabIndex = 12;
+            this.textBox_ModCity.TextChanged += new System.EventHandler(this.TextBox_ModCity_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(296, 201);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Codul Universitatii:";
+            // 
+            // textBox_ModCodeUniv
+            // 
+            this.textBox_ModCodeUniv.Location = new System.Drawing.Point(298, 217);
+            this.textBox_ModCodeUniv.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox_ModCodeUniv.Name = "textBox_ModCodeUniv";
+            this.textBox_ModCodeUniv.Size = new System.Drawing.Size(91, 20);
+            this.textBox_ModCodeUniv.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(191, 201);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Orasul:";
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.Location = new System.Drawing.Point(298, 132);
+            this.button_Delete.Margin = new System.Windows.Forms.Padding(2);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(90, 34);
+            this.button_Delete.TabIndex = 16;
+            this.button_Delete.Text = "Sterge";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            this.button_Delete.Click += new System.EventHandler(this.Button_Delete_Click);
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(46, 249);
+            this.button_Add.Margin = new System.Windows.Forms.Padding(2);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(90, 34);
+            this.button_Add.TabIndex = 17;
+            this.button_Add.Text = "Adauga";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.Button_Add_Click);
+            // 
+            // button_Modify
+            // 
+            this.button_Modify.Location = new System.Drawing.Point(257, 249);
+            this.button_Modify.Margin = new System.Windows.Forms.Padding(2);
+            this.button_Modify.Name = "button_Modify";
+            this.button_Modify.Size = new System.Drawing.Size(90, 34);
+            this.button_Modify.TabIndex = 18;
+            this.button_Modify.Text = "Modifica";
+            this.button_Modify.UseVisualStyleBackColor = true;
+            this.button_Modify.Click += new System.EventHandler(this.Button_Modify_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonShadow;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.codeDataGridViewTextBoxColumn,
+            this.numeFacDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.facultatiBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(415, 281);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 41;
+            // 
+            // codeDataGridViewTextBoxColumn
+            // 
+            this.codeDataGridViewTextBoxColumn.DataPropertyName = "Code";
+            this.codeDataGridViewTextBoxColumn.HeaderText = "Code";
+            this.codeDataGridViewTextBoxColumn.Name = "codeDataGridViewTextBoxColumn";
+            this.codeDataGridViewTextBoxColumn.Width = 57;
+            // 
+            // numeFacDataGridViewTextBoxColumn
+            // 
+            this.numeFacDataGridViewTextBoxColumn.DataPropertyName = "NumeFac";
+            this.numeFacDataGridViewTextBoxColumn.HeaderText = "NumeFac";
+            this.numeFacDataGridViewTextBoxColumn.Name = "numeFacDataGridViewTextBoxColumn";
+            this.numeFacDataGridViewTextBoxColumn.Width = 78;
+            // 
+            // facultatiBindingSource
+            // 
+            this.facultatiBindingSource.DataMember = "Facultati";
+            this.facultatiBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // database1DataSetBindingSource
+            // 
+            this.database1DataSetBindingSource.DataSource = this.database1DataSet;
+            this.database1DataSetBindingSource.Position = 0;
+            // 
+            // facultatiTableAdapter
+            // 
+            this.facultatiTableAdapter.ClearBeforeFill = true;
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(172, 574);
+            this.button_update.Margin = new System.Windows.Forms.Padding(2);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(90, 34);
+            this.button_update.TabIndex = 20;
+            this.button_update.Text = "Update";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.Button_update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 615);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_Modify);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_ModCodeUniv);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_ModCity);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.textBox_CodeUniv);
+            this.Controls.Add(this.textBox_City);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox_Fac);
+            this.Controls.Add(this.listBox_Univ);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.facultatiBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox_Univ;
+        private System.Windows.Forms.ListBox listBox_Fac;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_City;
+        private System.Windows.Forms.TextBox textBox_CodeUniv;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_ModCity;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_ModCodeUniv;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_Modify;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource database1DataSetBindingSource;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource facultatiBindingSource;
+        private Database1DataSetTableAdapters.FacultatiTableAdapter facultatiTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numeFacDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button_update;
+    }
+}
+
---
+++ Lab 3/MyDatabase/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyDatabase
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection myCon = new SqlConnection();
+        DataSet dsUniv;
+        DataSet dsFac;
+
+        int numberOfFields=0;
+        public Form1()
+        {
+            InitializeComponent();
+            myCon.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB;
+                                        AttachDbFilename=C:\Users\Catalin\Documents\Lab-II\Lab 3\MyDatabase\Database1.mdf;
+                                        Integrated Security = True";
+            MyDataGridView();
+            ListRefresh();
+        }
+        public void ListRefresh()
+        {
+            numberOfFields = 0;
+            listBox_Univ.Items.Clear();
+            textBox_City.Clear();
+            textBox_CodeUniv.Clear();
+
+            myCon.Open();
+            dsUniv = new DataSet();
+            dsFac = new DataSet();
+
+            SqlDataAdapter daUniv = new SqlDataAdapter("SELECT * FROM Universitati", myCon);
+            daUniv.Fill(dsUniv, "Universitati");
+            SqlDataAdapter daFac = new SqlDataAdapter("SELECT * FROM Facultati", myCon);
+            daFac.Fill(dsFac, "Facultati");
+            foreach (DataRow dr in dsUniv.Tables["Universitati"].Rows)
+            {
+                numberOfFields++;
+                String name = dr.ItemArray.GetValue(1).ToString();
+                listBox_Univ.Items.Add(name);
+            }
+
+            myCon.Close();
+        }
+        DataTable table = new DataTable();
+        public void MyDataGridView()
+        {
+            myCon.Open();
+            //Creating the Data adapter
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Facultati", myCon))
+            {
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            
+            myCon.Close();
+        }
+        private void Button_update_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Facultati", myCon);
+            try
+            {
+                SqlCommandBuilder cmdbl = new SqlCommandBuilder(adapter);
+                adapter.Update(table);
+            }
+            catch (Exception exec)
+            {
+                MessageBox.Show("Cod invalid!");
+            }
+            ListRefresh();
+        }
+        
+        private void ListBox_Univ_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            listBox_Fac.Items.Clear();
+            textBox_City.Clear();
+            int code = 0;
+            String UnivSelected = null;
+            try
+            {
+                    UnivSelected = listBox_Univ.SelectedItem.ToString();
+            }
+            catch (Exception exec)
+            {
+
+            }
+            if(UnivSelected != null)
+            {
+                foreach (DataRow dr in dsUniv.Tables["Universitati"].Rows)
+                {
+                    if (UnivSelected == dr.ItemArray.GetValue(1).ToString())
+                    {
+                        textBox_City.Text = dr.ItemArray.GetValue(2).ToString();
+                        code = Convert.ToInt16(dr.ItemArray.GetValue(3));
+                        textBox_CodeUniv.Text = code.ToString();
+                    }
+                }
+                foreach (DataRow dr in dsFac.Tables["Facultati"].Rows)
+                {
+                    if (code == Convert.ToInt16(dr.ItemArray.GetValue(1)))
+                    {
+                        String nameFac = dr.ItemArray.GetValue(2).ToString();
+                        listBox_Fac.Items.Add(nameFac);
+                    }
+                }
+            }
+        }
+        private void Button_Delete_Click(object sender, EventArgs e)
+        {
+            myCon.Open();
+
+            // Create the DeleteCommand.
+            SqlCommand command = new SqlCommand("DELETE FROM Universitati WHERE [NameUniv] LIKE @name", myCon);
+
+            String UnivSelected = null;
+            try
+            {
+                UnivSelected = listBox_Univ.SelectedItem.ToString();
+            }
+            catch (Exception exec)
+            {
+                MessageBox.Show("Selecteaza o universitate!");
+            }
+            if(UnivSelected != null)
+            {
+                // Add the parameters for the DeleteCommand.
+                SqlParameter parameter = command.Parameters.AddWithValue("@name", UnivSelected);
+                parameter.SourceVersion = DataRowVersion.Original;
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception exec)
+                {
+                    MessageBox.Show("Eroare gasita si ignorata!");
+                }
+            }
+            myCon.Close();
+
+            ListRefresh();
+        }
+        private void Button_Modify_Click(object sender, EventArgs e)
+        {
+            myCon.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE Universitati SET NameUniv = @name, City = @city , Code = @code " +
+                                                 "WHERE NameUniv LIKE @oldName", myCon);
+
+            // Add the parameters for the UpdateCommand.
+            command.Parameters.AddWithValue("@name", textBox_Name.Text);
+            command.Parameters.AddWithValue("@city", textBox_ModCity.Text);
+            command.Parameters.AddWithValue("@code", textBox_ModCodeUniv.Text);
+
+            String UnivSelected = null;
+            try
+            {
+                UnivSelected = listBox_Univ.SelectedItem.ToString();
+            }
+            catch (Exception exec)
+            {
+                MessageBox.Show("Selecteaza o universitate!");
+            }
+            if (UnivSelected != null)
+            {
+                SqlParameter parameter = command.Parameters.AddWithValue("@oldName", UnivSelected);
+                parameter.SourceVersion = DataRowVersion.Original;
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception exec)
+                {
+                    MessageBox.Show("Optiune invalida");
+                    throw;
+                }
+            }
+            myCon.Close();
+
+            ListRefresh();
+        }
+        private void Button_Add_Click(object sender, EventArgs e)
+        {
+            myCon.Open();
+            // Create the InsertCommand.
+            SqlCommand command = new SqlCommand("INSERT into Universitati([Id],[NameUniv],[City],[Code]) values(@id, @name, @city, @code)", myCon);
+
+            // Add the parameters for the InsertCommand.
+
+            command.Parameters.AddWithValue("@id", numberOfFields);
+            command.Parameters.AddWithValue("@name", textBox_Name.Text);
+            command.Parameters.AddWithValue("@city", textBox_ModCity.Text);
+            command.Parameters.AddWithValue("@code", textBox_ModCodeUniv.Text);
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception exec)
+            {
+                MessageBox.Show("Cod universitar invalid!");
+            }
+            
+            myCon.Close();
+
+            ListRefresh();
+        }
+        private void TextBox_Name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox_ModCity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
