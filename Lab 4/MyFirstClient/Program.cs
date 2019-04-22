bd204a3504449700f820c3c510115412830ab08f
AvramCatalin <49835245+AvramCatalin@users.noreply.github.com>
2019-04-22 23:35:07 +0300

    v1.4.1

---
+++ Lab 4/MyFirstClient/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstClient
+{
+    class Program
+    {
+        public static void TempConverter()
+        {
+            MyFirstClient.ServiceReference1.WebService1SoapClient service = new MyFirstClient.ServiceReference1.WebService1SoapClient();
+            Console.WriteLine("1)\nFahrenheit to Celsius (C)");
+            Console.WriteLine("Celsius to Fahrenheit (F)");
+            Console.Write("Please select an option: ");
+            char option = Console.ReadKey().KeyChar;
+            Console.Write("\nGive me a temperature: ");
+            float degrees = 0;
+            try
+            {
+                degrees = float.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong type of value given!");
+                option = 'x';
+            }
+            degrees = service.ConvertDegrees(degrees, option);
+
+            switch (option)
+            {
+                case 'F':
+                    Console.WriteLine("Celsius to Fahrenheit: " + degrees.ToString("N3") + " F\u00B0");
+                    break;
+                case 'C':
+                    Console.WriteLine("Fahrenheit to Celsius: " + degrees.ToString("N3") + " C\u00B0");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void TimeAndDate()
+        {
+            MyFirstClient.ServiceReference1.WebService1SoapClient service = new MyFirstClient.ServiceReference1.WebService1SoapClient();
+            Console.WriteLine("\n" + "2)\n" + service.CurrentDateTime());
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void FiveList()
+        {
+            MyFirstClient.ServiceReference1.WebService1SoapClient service = new MyFirstClient.ServiceReference1.WebService1SoapClient();
+            var list = new List<int>();
+            Console.WriteLine("\n" + "3)");
+            list = service.ReturnList();
+            list.ForEach(i => Console.Write("{0}\t", i));
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void MoneyConverter()
+        {
+            MyFirstClient.ServiceReference1.WebService1SoapClient service = new MyFirstClient.ServiceReference1.WebService1SoapClient();
+            Console.WriteLine("4)\nEuro to Ron (R)");
+            Console.WriteLine("Ron to Euro (E)");
+            Console.Write("Please select an option: ");
+            char option = Console.ReadKey().KeyChar;
+            Console.Write("\nGive me a sum of money: ");
+            float sum = 0;
+            try
+            {
+                sum = float.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong type of value given!");
+                option = 'x';
+            }
+            sum = service.ConvertMoney(sum, option);
+
+            switch (option)
+            {
+                case 'R':
+                    Console.WriteLine("Euro to Ron: " + sum.ToString("N3") + " Ron");
+                    break;
+                case 'E':
+                    Console.WriteLine("Ron to Euro: " + sum.ToString("N3") + " Euro");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main(string[] args)
+        {
+            int option = 0;
+            while(option != 5)
+            {
+                Console.WriteLine("1)Temperature Converter");
+                Console.WriteLine("2)Display date and time");
+                Console.WriteLine("3)Display the first 5 prime numbers");
+                Console.WriteLine("4)Money Converter");
+                Console.WriteLine("5)Exit");
+                Console.Write("\nSelect and option: ");
+                try
+                {
+                    option=int.Parse(Console.ReadLine());
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("Wrong type of value given!");
+                }
+                switch (option)
+                {
+                    case 1:
+                        TempConverter();
+                        break;
+                    case 2:
+                        TimeAndDate();
+                        break;
+                    case 3:
+                        FiveList();
+                        break;
+                    case 4:
+                        MoneyConverter();
+                        break;
+                    case 5:
+                        option = 5;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Lab 4/MyFirstServices/WebService1.asmx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Services;
+
+namespace MyFirstServices
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public float ConvertDegrees(float degrees,char option)
+        {
+                switch (option)
+                {
+                    case 'F':
+                        degrees = (degrees * 1.8F) + 32;
+                        break;
+                    case 'C':
+                        degrees = (degrees - 32) * (0.555F);
+                        break;
+                    default:
+                        break;
+                }
+            return degrees;
+        }
+        [WebMethod]
+        public String CurrentDateTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        [WebMethod]
+        public List<int> ReturnList()
+        {
+            var list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(5);
+            list.Add(7);
+            list.Add(11);
+            return list;
+        }
+        [WebMethod]
+        public float ConvertMoney(float sum, char option)
+        {
+            switch (option)
+            {
+                case 'R':
+                    sum = (sum * 4.76F);
+                    break;
+                case 'E':
+                    sum = (sum * 0.21F);
+                    break;
+                default:
+                    break;
+            }
+            return sum;
+        }
+    }
+}
