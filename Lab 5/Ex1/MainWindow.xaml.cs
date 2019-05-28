c912e3f1a671adc87fb988b59a8fd01e679c407d
Avram Cătălin <49835245+AvramCatalin@users.noreply.github.com>
2019-05-28 22:12:32 +0300

    v1.5.1 Ex 1 done

---
+++ Lab 5/Ex1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"Login.txt");
+            string username = lines[0];
+            string password = lines[1];
+            if(username==textBox_username.Text && password == passwordBox_password.Password)
+            {
+                Welcome welcome = new Welcome(username);
+                welcome.Show();
+                this.Close();
+            }
+        }
+    }
+}
